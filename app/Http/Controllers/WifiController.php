342924eb6c0ec244e6282ff61b55141de95f9d9e
Ryuze <alfianyunianto74@gmail.com>
2022-01-19 09:54:54 +0700

    model and userseeder added

---
+++ app/Http/Controllers/WifiController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Wifi;
+use App\Http\Requests\StoreWifiRequest;
+use App\Http\Requests\UpdateWifiRequest;
+
+class WifiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreWifiRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreWifiRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Wifi  $wifi
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Wifi $wifi)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Wifi  $wifi
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Wifi $wifi)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateWifiRequest  $request
+     * @param  \App\Models\Wifi  $wifi
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateWifiRequest $request, Wifi $wifi)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Wifi  $wifi
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Wifi $wifi)
+    {
+        //
+    }
+}
---
+++ database/seeders/WifiSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class WifiSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
